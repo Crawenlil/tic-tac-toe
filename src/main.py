a117aa1cb90b49d9d69e92ce86425e2aaeb563ec
Tomxey <tpastusiak@gmail.com>
2018-05-28 19:11:07 +0200

    play_game_no_ui

--- src/main.py
+++ src/main.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 from game_executor import GameExecutor 
-from player import HumanPlayer, QPlayer
+from player import HumanPlayer, QPlayer, RandomPlayer
 
 players = {
     'Human' : HumanPlayer,
     'Q' : QPlayer
 }
 
+def train_player(board_size, train_set_size):
+    player = RandomPlayer("random")
+    game_executor = GameExecutor(player, player)
+    train = game_executor.prepare_train_set(board_size=board_size, n_games=train_set_size)
+    import pdb; pdb.set_trace()
+
 def main():
-    player_x = HumanPlayer("Player X")
-    player_o = HumanPlayer("Player O")
+    # train_player(3, 10)
+    player_x = RandomPlayer("Player X")
+    player_o = RandomPlayer("Player O")
     game_executor = GameExecutor(player_x, player_o)
-    game_executor.play(board_size=3, n_games=1, with_ui=True)
+    game_executor.play(board_size=3, n_games=1, with_ui=False)
 
 
 if __name__ == '__main__':
--- src/player.py
+++ src/player.py
@@ -1,4 +1,6 @@
 import game_engine
+import random
+import copy
 
 class Player(object):
 
@@ -21,6 +23,28 @@ class Player(object):
 
 class HumanPlayer(Player):
     pass
+
+class RandomPlayer(Player):
+    def make_move(self, state):
+        actions = game_engine.actions(state)
+        action = random.choice(actions)
+        game_engine.make_move(state, action)
+
+class SupervisedLearningPlayer(Player):
+    def __init__(self, name, classifier):
+        self.name = name
+        self.classifier = classifier
+
+    def make_move(self, state):
+        actions = game_engine.actions(state)
+        applied_actions = []
+        for action in actions:
+            tmp_state = copy.deepcopy(state)
+            game_engine.make_move(tmp_state, action)
+            applied_actions.append(tmp_state)
+        action_scores = self.classifier.predict(applied_actions)
+        return actions[0]
+
 
 class QPlayer(Player):
     pass
